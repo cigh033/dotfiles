4ead4d4fa35cb9bf12ef1c33a5e71919f1416af7
sb <mail@cigh.de>
2019-10-25 15:36:31 +0200

    changed brightness fuckup 2

--- .config/i3/autolock.sh
+++ .config/i3/autolock.sh
@@ -16,7 +16,7 @@ export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 	'~/.config/i3/brightnesscontrol.sh 20' \
 	'pkill brightnesscontr' \
 	--timer primary 90 \
-	'pkill brightnesscontr;$HOME/.config/i3/i3lock.sh;~/.config/i3/brightnesscontrol.sh 10' \
+	'$HOME/.config/i3/i3lock.sh;pkill brightnesscontr;~/.config/i3/brightnesscontrol.sh 1' \
 	'pkill brightnesscontr' \
 	--timer normal 1800 \
 	'$HOME/.config/i3/autosuspend.sh' \
